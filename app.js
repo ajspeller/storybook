99fd6db61c6aaa5cbe978599a7a52c91d16a47f9
AJ Speller <ajspeller@gmail.com>
2018-09-26 23:32:10 -0400

    Added Google Authentication

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// passport config
+require('./config/passport')(passport);
+
+// load router
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Index');
 });
+
+app.use('/auth', authRoutes);
 
 const port = process.env.PORT || 5000;
 
