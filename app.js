e0ff52bcc735213a5b878b5b491335f8d09a47cd
AJ Speller <ajspeller@gmail.com>
2018-09-27 00:25:29 -0400

    All user authentication code added

--- app.js
+++ app.js
@@ -2,19 +2,59 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+// load User model
+require('./models/User');
 
 // passport config
 require('./config/passport')(passport);
 
-// load router
-const authRoutes = require('./routes/auth');
+
+// mongoose connect
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log(`Database connection successful!`)
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 const app = express();
 
+app.use(cookieParser());
+
+// express session
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+
+// passport middleware, has to be after express session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+// index route
 app.get('/', (req, res) => {
   res.send('Index');
 });
+
+
+// load router
+const authRoutes = require('./routes/auth');
 
 app.use('/auth', authRoutes);
 
