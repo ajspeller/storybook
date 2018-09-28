e3a3472a464ec4ad4e2c5f4895c502fbc146a5e5
AJ Speller <ajspeller@gmail.com>
2018-09-28 01:44:31 -0400

    Added middleware to protect routes from anonymous users

--- routes/stories.js
+++ routes/stories.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+  ensureAuthenticated
+} = require('../helpers/auth');
+
 // stories index
 router.get('/', (req, res) => {
   res.render('stories/index');
 });
 
 // add story form
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('stories/add');
 });
 
